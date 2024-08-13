d397563e5a007d768992f72cdd04a86cb0d6a34c
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-08-13 12:52:34 +0300

    fix

--- users-generator/users-generator.sh
+++ users-generator/users-generator.sh
@@ -4,27 +4,29 @@
 USERS_YAML=/data/users.yml
 
 # Initialize the users.yaml file
-echo "users:" > $USERS_YAML
+echo "users:" > "$USERS_YAML"
 
-# Split the DOZZLE_USERS and DOZZLE_PASSWORDS into arrays
+# Split the DOZZLE_USERS, DOZZLE_PASSWORDS, and DOZZLE_EMAILS into arrays
 IFS="," read -ra USERS <<< "$DOZZLE_USERS"
-IFS="," read -ra PASSWORDS <<< "$DOZZLE_PASSES"
+IFS="," read -ra PASSWORDS <<< "$DOZZLE_PASSWORDS"
+IFS="," read -ra EMAILS <<< "$DOZZLE_EMAILS"
 
-echo $DOZZLE_PASSES
-echo $DOZZLE_USERS
-
-# Check if the number of users matches the number of passwords
-if [ ${#USERS[@]} -ne ${#PASSWORDS[@]} ]; then
-  echo "Error: The number of users and passwords must match."
+# Check if the number of users matches the number of passwords and emails
+if [ ${#USERS[@]} -ne ${#PASSWORDS[@]} ] || [ ${#USERS[@]} -ne ${#EMAILS[@]} ]; then
+  echo "Error: The number of users, passwords, and emails must match."
   exit 1
 fi
 
-# Loop through users and passwords and append them to the YAML file
+# Loop through users, passwords, and emails and append them to the YAML file
 for i in "${!USERS[@]}"; do
-  echo "  - username: ${USERS[i]}" >> $USERS_YAML
-  echo "    password: ${PASSWORDS[i]}" >> $USERS_YAML
-done
+  USERNAME=${USERS[i]}
+  PASSWORD_HASH=$(echo -n "${PASSWORDS[i]}" | sha256sum | awk '{print $1}')
+  EMAIL=${EMAILS[i]}
 
-cat /data/users.yml
+  echo "  ${USERNAME}:" >> "$USERS_YAML"
+  echo "    name: \"${USERNAME^}\"" >> "$USERS_YAML" # Capitalize the first letter
+  echo "    password: \"${PASSWORD_HASH}\"" >> "$USERS_YAML"
+  echo "    email: \"${EMAIL}\"" >> "$USERS_YAML"
+done
 
 echo "Generated users.yml with ${#USERS[@]} users."
